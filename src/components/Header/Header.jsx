ed4b99dd6375c425d6cf91a53a6ad1c32caa3315
MrJanHorak <janny_b_goode@hotmail.com>
2022-11-12 18:51:43 -0500

    begin styling of header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Header.css'
 
 function Header() {
   return (
-    <div>
-      Header
+    <div className='headerContainer'>
+      <div className='time'>12:00 AM</div>
+      <div className='alert'> ALERT / NOTIFICATION / TICKER</div>
+      <div className='date'>12 November 2022</div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
