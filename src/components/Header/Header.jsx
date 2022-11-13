15fd4abef567cc676aa5e1c007bc6a0f50f37240
MrJanHorak <janny_b_goode@hotmail.com>
2022-11-13 17:47:58 -0500

    add date and time to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import './Header.css'
+import React, { useEffect, useState } from 'react';
+import { AiOutlineClockCircle, AiOutlineCalendar } from 'react-icons/ai';
+import './Header.css';
 
 function Header() {
+  const [dateState, setDateState] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => setDateState(new Date()), 1000);
+  }, []);
+
   return (
     <div className='headerContainer'>
-      <div className='time'>12:00 AM</div>
+      <div className='time'>
+        <AiOutlineClockCircle />
+        {' '}
+          {dateState.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+            second: 'numeric',
+          })}
+      </div>
       <div className='alert'> ALERT / NOTIFICATION / TICKER</div>
-      <div className='date'>12 November 2022</div>
+      <div className='date'>
+        <AiOutlineCalendar />
+          {' '}
+          {dateState.toLocaleDateString('en-GB', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+          })}
+      </div>
     </div>
   );
 }
